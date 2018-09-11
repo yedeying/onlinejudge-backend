fc0b65f0db0a7b8d94be97fa97e853d91e4224ec
yedeying <yedeying999@gmail.com>
2018-09-11 09:52:48 +0000

    feat: initial project

---
+++ src/server.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Koa = require("koa");
+const Router = require("koa-router");
+const app = new Koa();
+const router = new Router();
+router.get('/*', async (ctx) => {
+    ctx.body = 'Hello World!';
+});
+app.use(router.routes());
+app.listen(3000);
+console.log('Server running on port 3000');
